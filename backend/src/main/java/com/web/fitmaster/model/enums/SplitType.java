6ba4d688e9f2007e7e6508087e319cf64136b26f
Abdalkrem2 <abdalkremalshbol8@gmail.com>
2026-04-17 21:15:12 +0300

    implement the workout plan logic V1

---
+++ backend/src/main/java/com/web/fitmaster/model/enums/SplitType.java
@@ -0,0 +1,9 @@
+package com.web.fitmaster.model.enums;
+
+public enum SplitType {
+    FULL_BODY,        // 3 أيام
+    UPPER_LOWER,      // 4 أيام
+    BRO_SPLIT_4DAY,   // 4 أيام: Chest+Tri / Back+Bi / Shoulders / Legs
+    BRO_SPLIT_5DAY,   // 5 أيام: Chest / Back / Shoulders / Legs / Arms
+    PUSH_PULL_LEGS    // 6 أيام
+}
